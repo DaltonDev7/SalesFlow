bb6c11e8f68590d84cca43078bd2bd1dd76f581d
DaltonDev7 <daltontejada28@gmail.com>
2025-03-09 21:27:30 -0400

    feat: creando vista producto

---
+++ SalesFlowApp/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesFlowApp.Models
+{
+    public class Category
+    {
+        public int? Id { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? LastModified { get; set; }
+
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+    }
+}
---
+++ SalesFlowApp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace SalesFlowApp.Models
+{
+    public class Product
+    {
+        public int? Id { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? LastModifiedBy { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? LastModified { get; set; }
+        public int? Status { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int IdCategory { get; set; }
+        public Boolean Available { get; set; } = true;
+    }
+}
